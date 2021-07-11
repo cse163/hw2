034e023bf4bb6f4a94075b4dd829c4aead3c3d8b
wenq97 <wenq97@uw.edu>
2021-07-11 15:12:53 -0700

    Added starter files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# hw2
-
+This repo has starter code for a CSE 163 assignment run in GitHub classroom.
---
+++ hw2_test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+from cse163_utils import assert_equals, parse
+
+import hw2_manual
+import hw2_pandas
+
+
+# Your tests here!
+
+
+def main():
+    # Call your test functions here!
+
+
+if __name__ == '__main__':
+    main()
